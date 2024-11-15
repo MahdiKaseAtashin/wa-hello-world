813397571dd592e35b3806972eb84f16a560f885
MahdiKaseAtashin <MahdiKaseAtashin@gmail.com>
2024-11-15 16:49:24 +0330

    + add about page

--- main.go
+++ main.go
@@ -5,15 +5,35 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprintf(w, "Hello, world")
-		if err != nil {
-			fmt.Println("Error writing response:", err)
-		}
-	})
+const portNumber = "8080"
 
-	err := http.ListenAndServe(":7551", nil)
+// Home is the about page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "This is the home page")
+	if err != nil {
+		fmt.Println("Error writing response in home:", err)
+	}
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, err := fmt.Fprintf(w, "This is the about page and the sum  is %d", sum)
+	if err != nil {
+		fmt.Println("Error writing response in home:", err)
+	}
+}
+
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	err := http.ListenAndServe(":"+portNumber, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
