dff93e871e00526051e66bc5ee2a6d9f2abe85f1
MahdiKaseAtashin <MahdiKaseAtashin@gmail.com>
2024-11-15 16:25:30 +0330

    + create a server and run on 7551 port

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, world")
+		if err != nil {
+			fmt.Println("Error writing response:", err)
+		}
+	})
+
+	err := http.ListenAndServe(":7551", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 
 }
